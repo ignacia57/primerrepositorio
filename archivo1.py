9e5639223fd7e8dcb0c24eac972c2853d068b527
Ignacia Moya <ignaciia57@gmail.com>
2020-09-06 19:07:55 -0300

    saludo

---
+++ archivo1.py
@@ -0,0 +1,10 @@
+x=3+2
+print(f'La suma de 3+2 es {x}')
+y=3*2
+print(f'La multiplicaciòn de 3*2 es {y}')
+z=9/4
+print(f'La division de 9/4 es {z}')
+W=9-4
+print(f'La resta de 9-4 es {w}')
+Y=9%4
+print(f'el modulo de 9%4 es {y}')
