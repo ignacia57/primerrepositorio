f2d8204702aa3a4f252b09583b3cf28c2b7803b1
Ignacia Moya <ignaciia57@gmail.com>
2020-09-06 21:50:59 -0300

    archivo2

---
+++ archivo2.py
@@ -0,0 +1,7 @@
+diccionario={'nombre': 'Ignacia', 'apellido': 'Moya', 'nota programacion': 7.0, 'nota fisica': 6.0}
+diccionario1={'nombre': 'Ivvy', 'apellido': 'Cifu', 'nota programacion': 5.4, 'nota fisica': 6.0}
+diccionario2={'nombre': 'Bayron', 'apellido': 'Tobar', 'nota programacion': 6.9, 'nota fisica': 5.7}
+diccionario3={'nombre': 'Ignacia', 'apellido': 'Soto', 'nota programacion': 7.0, 'nota fisica': 7.0}
+
+print(f' la respuesta a diccionario es {'nombre': 'Ignacia', 'apellido': 'Moya', 'nota programacion': 7.0, 'nota  fisica': 6.0}')
+
